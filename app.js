f1dae13101cbd6a6f2dbe007fcb289b9ec0ed596
Carlitos <carlitosdutan@pursuit.org>
2024-02-20 13:22:28 -0500

    finished lab

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.get("/frakenstein", (req, res) => {
 });
 
 app.get("/rocky", (req, res) => {
-  res.send("Yo Adrian");
+  res.send("Yo, Adrian");
 });
 
 app.get("/dorothy", (req, res) => {
@@ -38,6 +38,35 @@ app.get("/dorothy", (req, res) => {
 
 app.get("/james-bond", (req, res) => {
   res.send("the name is Bond, James Bond");
+});
+
+// magic 8 ball activity:
+
+const magic8Responses = [
+  "It is certain",
+  "It is decidedly so",
+  "Without a doubt",
+  "Yes - Definitely",
+  "You may rely on it",
+  "As I see it, yes",
+  "Most likely",
+  "Outlook good",
+  "Yes, and signs point to yes",
+  "Reply hazy, try again",
+  "Ask again later",
+  "Better not tell you now",
+  "Cannot predict now",
+  "Concentrate and ask again",
+  "Don't count on it",
+  "My reply is no",
+  "My sources say no",
+  "Outlook not so good",
+  "Very doubtful",
+];
+
+app.get("/magic8", (req, res) => {
+  const randomIndex = Math.floor(Math.random() * magic8Responses.length);
+  res.send(`<h1>${magic8Responses[randomIndex]}</h1>`);
 });
 
 const PORT = process.env.PORT || 3003;
