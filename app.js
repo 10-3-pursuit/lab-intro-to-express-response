e55ecbd314264a13a8be6ee2f1e404d8007e8844
juligarc91 <julissagarcia@pursuit.org>
2024-02-20 19:41:02 -0500

    created server.js and test

--- app.js
+++ app.js
@@ -125,13 +125,6 @@ app.get('/magic8', (req, res) => {
     `);
 });
 
-// ---- Listening for URL res/req on Port ----
-const PORT = process.env.PORT || 3003; // saves PORT to a variable so we can use in a callback fx in next lines that also outputs a message to the console for user; however installing nodemon and running nodemon app.js in the terminal shows us a message to let us know the port is listening for requests / responses and updates as this server gets updated 
-
-// app.listen(3003); // this code snippet doesn't give us a way to console.log() a message in terminal like the one in the next line does, but it is all that we need
-app.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}...`)
-})
 // --- part of the bonus ---
 // for test to run: we want app from the app.js file exported.
 module.exports = app;
